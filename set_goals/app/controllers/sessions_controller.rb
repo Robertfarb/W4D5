93fdbf7554801daff89ebaf0eacd852f20bdb823
Robert Farb <farb.robert@gmail.com>
2018-07-13 15:17:55 -0700

    Complete Sessions Controller

--- set_goals/app/controllers/sessions_controller.rb
+++ set_goals/app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username],
+    params[:user][:password])
+
+    if @user
+      log_in_user(@user)
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = ['Incorrect Username or Password']
+      render :new
+    end
+  end
+
+  def destroy
+    logout_user(current_user)
+    redirect_to new_session_url
+  end
+
+  def new
+    @user = user.new
+    render :new
+  end
 end
