0a76474cb1dbbedfcf9474d44133544209d49cb1
Robert Farb <farb.robert@gmail.com>
2018-07-13 12:10:14 -0700

    Create Users migration and add index to users table

---
+++ set_goals/db/migrate/20180713190758_add_index_to_session_token.rb
@@ -0,0 +1,6 @@
+class AddIndexToSessionToken < ActiveRecord::Migration[5.1]
+  def change
+    add_index :users, :session_token, unique: true
+    add_index :users, :username, unique: true
+  end
+end
--- set_goals/spec/models/user_spec.rb
+++ set_goals/spec/models/user_spec.rb
@@ -1,5 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
+requre 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe UserModel do
+    it 'validates presence of username' do
+    end
+
+    it 'validates presence of password digest' do
+    end
+
+    it 'validates presence of password' do
+    end
+  end
 end
