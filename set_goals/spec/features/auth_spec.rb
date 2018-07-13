16548c43977665702599dea4fdcb892d5b92119a
Robert Farb <farb.robert@gmail.com>
2018-07-13 16:32:24 -0700

    Complete signup process capybara tests

--- set_goals/spec/features/auth_spec.rb
+++ set_goals/spec/features/auth_spec.rb
@@ -9,21 +9,32 @@ feature 'the signup process' do
   end
 
   feature 'signing up a user' do
+    before(:each) do
+      visit new_user_url
+      fill_in 'username', with: 'testing_username'
+      fill_in 'password', with: 'biscuits'
+      click_on 'Create User'
+    end
+
+    scenario 'redirects to user profile page after sign-up' do
+      expect(page).to have_content 'Welcome, testing_username'
+    end
   end
 
   scenario 'shows username on the homepage after signup' do
   end
-  
 end
 
 feature 'logging in' do
-  scenario 'shows username on the homepage after login'
-
+  scenario 'shows username on the homepage after login' do
+  end
 end
 
 feature 'logging out' do
-  scenario 'begins with a logged out state'
+  scenario 'begins with a logged out state' do
+  end
 
-  scenario 'doesn\'t show username on the homepage after logout'
+  scenario 'doesn\'t show username on the homepage after logout' do
+  end
 
 end
